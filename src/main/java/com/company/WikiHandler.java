42984c29f7ff1e52fc03bad4de7a37c47b2ae1c6
Danila Iagupets <d.iagupets@gmail.com>
2021-04-08 21:11:01 +0300

    Added xml structure check
    Added xml structure check. Incorrect page is no longer included in stats.

--- src/main/java/com/company/WikiHandler.java
+++ src/main/java/com/company/WikiHandler.java
@@ -1,7 +1,6 @@
 package com.company;
 
 import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 import java.time.Instant;
@@ -10,64 +9,95 @@ import java.time.ZoneOffset;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class WikiHandler extends DefaultHandler {
-
+    //TODO Split parsing and counting stats into different threads
+    private final WikiStats stats;
+    private final StringBuilder buffer;
     private int counter = 0;
-    private WikiStats stats;
-    private StringBuilder buffer = new StringBuilder();
-
-    public WikiStats getStats() {
-        return stats;
-    }
+    private final StringBuilder tagPath = new StringBuilder();
+    private boolean hasSizeAttribute = false;
+    private String text;
+    private String title;
+    private String timestamp;
+    private int size;
 
     public WikiHandler(WikiStats stats) {
         this.stats = stats;
+        this.buffer = new StringBuilder();
     }
 
     @Override
-    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+    public void startElement(String uri, String localName, String qName, Attributes attributes) {
         switch (qName) {
             case "page":
+                // Reset Path at the beginning of new page
+                tagPath.setLength(0);
+                hasSizeAttribute = false;
                 counter++;
                 break;
             case "title":
-                buffer = new StringBuilder();
+                break;
+            case "revision" :
                 break;
             case "timestamp":
-                buffer = new StringBuilder();
                 break;
             case "text":
-                sizeProcess(Integer.parseInt(attributes.getValue("bytes")));
-                buffer = new StringBuilder();
+                size = Integer.parseInt(attributes.getValue("bytes"));
+                //TODO size tag check
+                hasSizeAttribute=true;
                 break;
             default:
                 break;
         }
+        tagPath.append(qName).append(" ");
+        buffer.setLength(0); //Clear buffer
     }
 
     @Override
-    public void endElement(String uri, String localName, String qName) throws SAXException {
+    public void endElement(String uri, String localName, String qName) {
+        tagPath.append("/").append(qName).append(" ");
         switch (qName) {
             case "page":
                 if(counter%1000==0) {
                     System.out.println(Thread.currentThread().getName() + "   " + counter);
                 }
+                if(isValidPage()) {
+                    countStats();
+                }
                 break;
             case "title":
-                titleProcess(buffer.toString());
+                title = buffer.toString();
                 break;
             case "timestamp":
-                timestampProcess(buffer);
+                timestamp = buffer.toString();
                 break;
             case "text":
-                textProcess(buffer.toString());
-                break;
-            default:
+                text = buffer.toString();
                 break;
         }
     }
 
-    private void textProcess(String buffer) {
-        var words = buffer.split(" ");
+    @Override
+    public void characters(char[] ch, int start, int length) {
+        buffer.append(ch, start, length);
+    }
+
+    private boolean isValidPage() {
+        boolean tagMatch = tagPath.toString()
+                .equals("page title /title revision timestamp /timestamp text /text /revision /page ");
+        if(!(tagMatch && hasSizeAttribute)) {
+            System.out.println("Invalid page " + counter);
+        }
+        return tagMatch && hasSizeAttribute;
+    }
+    private void countStats() {
+        textProcess(text);
+        titleProcess(title);
+        timestampProcess(timestamp);
+        sizeProcess(size);
+    }
+
+    private void textProcess(String text) {
+        var words = text.split(" ");
         for(String word : words) {
             if(word.matches("[а-яА-я]{4,}")) {
                 word = word.toLowerCase();
@@ -77,8 +107,8 @@ public class WikiHandler extends DefaultHandler {
         }
     }
 
-    private void titleProcess(String buffer) {
-        var words = buffer.split(" ");
+    private void titleProcess(String title) {
+        var words = title.split(" ");
         for(String word : words) {
             if(word.matches("[а-яА-я]{4,}")) {
                 word = word.toLowerCase();
@@ -88,9 +118,8 @@ public class WikiHandler extends DefaultHandler {
         }
     }
 
-    private void timestampProcess(StringBuilder buffer) {
-        String input = buffer.toString();
-        Instant i = Instant.parse(input);
+    private void timestampProcess(String timestamp) {
+        Instant i = Instant.parse(timestamp);
         OffsetDateTime time = i.atOffset(ZoneOffset.UTC);
         Integer year = time.getYear();
 
@@ -110,10 +139,5 @@ public class WikiHandler extends DefaultHandler {
 
         stats.getSizeSpread().putIfAbsent(key, new AtomicLong(0));
         stats.getSizeSpread().get(key).incrementAndGet();
-    }
-
-    @Override
-    public void characters(char[] ch, int start, int length) throws SAXException {
-        buffer.append(ch, start, length);
     }
 }
