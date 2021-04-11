7113db476668fe3a247d2a2fc968169c309f326f
Danila Iagupets <d.iagupets@gmail.com>
2021-04-11 23:54:36 +0300

    Code cleanup

--- src/main/java/com/company/WikiHandler.java
+++ src/main/java/com/company/WikiHandler.java
@@ -1,7 +1,6 @@
 package com.company;
 
 import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 import java.time.Instant;
@@ -11,13 +10,11 @@ import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class WikiHandler extends DefaultHandler {
-    private final static int THREAD_SIZE = 16;
-    //TODO Split parsing and counting stats into different threads
+    private final static int THREAD_SIZE = Runtime.getRuntime().availableProcessors();
     private final WikiStats stats;
     private final ExecutorService executorService = Executors.newFixedThreadPool(THREAD_SIZE);
-    Semaphore lock = new Semaphore(THREAD_SIZE * 2);
+    private final Semaphore lock = new Semaphore(THREAD_SIZE * 2);
     private final StringBuilder buffer;
-    private int id = 0;
     private final StringBuilder tagPath = new StringBuilder();
     private boolean hasSizeAttribute = false;
 
@@ -27,10 +24,10 @@ public class WikiHandler extends DefaultHandler {
     private int size;
 
     class Task implements Runnable {
-        private String taskText;
-        private String taskTitle;
-        private String taskTimestamp;
-        private int taskSize;
+        private final String taskText;
+        private final String taskTitle;
+        private final String taskTimestamp;
+        private final int taskSize;
 
         public Task(String text, String title, String timestamp, int size) {
             this.taskText = text;
@@ -104,21 +101,16 @@ public class WikiHandler extends DefaultHandler {
     }
 
     @Override
-    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+    public void startElement(String uri, String localName, String qName, Attributes attributes) {
         switch (qName) {
             case "page":
                 // Reset Path at the beginning of new page
                 tagPath.setLength(0);
                 tagPath.append(qName).append(" ");
                 hasSizeAttribute = false;
-                id++;
                 break;
             case "title":
-                tagPath.append(qName).append(" ");
-                break;
             case "revision" :
-                tagPath.append(qName).append(" ");
-                break;
             case "timestamp":
                 tagPath.append(qName).append(" ");
                 break;
@@ -139,9 +131,6 @@ public class WikiHandler extends DefaultHandler {
         switch (qName) {
             case "page":
                 tagPath.append("/").append(qName);
-                if(id %1000==0) {
-                    System.out.println(Thread.currentThread().getName() + "   " + id);
-                }
                 if(isValidPage()) {
                     try {
                         lock.acquire();
@@ -182,79 +171,19 @@ public class WikiHandler extends DefaultHandler {
     }
 
     private boolean isValidPage() {
-        //Current implementation is invalid
-        //TODO Update validation via xsd file
+        //TODO Rewrite validation. Current implementation may not be valid for different tags inside page
         boolean tagMatch = tagPath.toString()
                 .equals("page title /title revision timestamp /timestamp text /text /revision /page");
-        if(!(tagMatch && hasSizeAttribute)) {
-            System.out.println("Invalid page " + id);
-            System.out.println(tagPath);
-        }
         return tagMatch && hasSizeAttribute;
     }
 
     @Override
-    public void endDocument() throws SAXException {
+    public void endDocument(){
         executorService.shutdown();
         try {
             executorService.awaitTermination(24L, TimeUnit.HOURS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("File parsed");
-    }
-
-    private void countStats() {
-        textProcess(text);
-        titleProcess(title);
-        timestampProcess(timestamp);
-        sizeProcess(size);
-    }
-
-    private void textProcess(String text) {
-        text = text.replaceAll("[^а-яА-я]"," ");
-        var words = text.split(" ");
-        for(String word : words) {
-            if(word.matches("[а-яА-я]{4,}")) {
-                word = word.toLowerCase();
-                stats.getTextWordFrequency().putIfAbsent(word, new AtomicLong(0));
-                stats.getTextWordFrequency().get(word).incrementAndGet();
-            }
-        }
-    }
-
-    private void titleProcess(String title) {
-        title = title.replaceAll("[^а-яА-я]"," ");
-        var words = title.split(" ");
-        for(String word : words) {
-            if(word.matches("[а-яА-я]{4,}")) {
-                word = word.toLowerCase();
-                stats.getTitleWordFrequency().putIfAbsent(word, new AtomicLong(0));
-                stats.getTitleWordFrequency().get(word).incrementAndGet();
-            }
-        }
-    }
-
-    private void timestampProcess(String timestamp) {
-        Instant i = Instant.parse(timestamp);
-        OffsetDateTime time = i.atOffset(ZoneOffset.UTC);
-        Integer year = time.getYear();
-
-        stats.getYearSpread().putIfAbsent(year, new AtomicLong(0));
-        stats.getYearSpread().get(year).incrementAndGet();
-    }
-
-    private void sizeProcess(int bytes) {
-        int key = 0;
-        if(bytes!=0) {
-            while (bytes > 0) {
-                bytes /= 10;
-                key++;
-            }
-            key -= 1;
-        }
-
-        stats.getSizeSpread().putIfAbsent(key, new AtomicLong(0));
-        stats.getSizeSpread().get(key).incrementAndGet();
     }
 }
